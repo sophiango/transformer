3703a1a19c06d8fe99c7542cf1c1c8c2b5997092
Sophia Ngo <ngo_sophia@icloud.com>
2025-05-16 20:44:57 -0700

    define env variables

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss()],
+    define: {
+        'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+        'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+        'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY)
+    }
 })
