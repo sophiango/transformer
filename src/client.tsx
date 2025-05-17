fdca117a2b41520264da2f341d17074283729cf3
Sophia Ngo <ngo_sophia@icloud.com>
2025-05-16 23:23:52 -0700

    removed console.log

--- src/client.tsx
+++ src/client.tsx
@@ -5,7 +5,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 console.log("Supabase URL", supabaseUrl);
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-console.log("Supabase", supabase);
 // API URL
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
